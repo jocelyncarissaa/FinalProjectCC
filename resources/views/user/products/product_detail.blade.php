1a04de684748a3af21a2bcc0085ed8d8965f55f1
valerie-alexia <148068674+valerie-alexia@users.noreply.github.com>
2025-12-19 14:23:35 +0700

    Update product_detail.blade.php

--- resources/views/user/products/product_detail.blade.php
+++ resources/views/user/products/product_detail.blade.php
@@ -72,11 +72,16 @@
 
                        blade
                             @if($item->image_path)
-                                <img 
-                                    src="{{ env('AWS_BUCKET') }}/{{ $item->image_path }}" 
-                                    alt="{{ $item->name }}" 
-                                    class="max-h-full object-contain"
-                                >
+                                {{-- ✅ PAKAI FULL URL S3 BERIKUT --}}
+                                @if($item->image_path)
+                                    <img 
+                                        src="https://pharmaplus-img-2025.s3.us-east-1.amazonaws.com/{{ $item->image_path }}" 
+                                        alt="{{ $item->name }}" 
+                                        class="max-h-full object-contain"
+                                    >
+                                @else
+                                    <div class="text-gray-400 text-xs">No Image</div>
+                                @endif
                             @else
                                 <div class="text-gray-400 text-xs">No Image</div>
                             @endif
