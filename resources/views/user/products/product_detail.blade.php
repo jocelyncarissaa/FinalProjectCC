14a832af744ea7f8212639bf127bb1122d2af96c
valerie-alexia <148068674+valerie-alexia@users.noreply.github.com>
2025-12-19 14:06:11 +0700

    Update product_detail.blade.php

--- resources/views/user/products/product_detail.blade.php
+++ resources/views/user/products/product_detail.blade.php
@@ -70,19 +70,17 @@
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path></svg>
                         </div>
 
-                        {{-- Image Area --}}
-                        <div class="p-4 bg-gray-100 flex justify-center items-center h-48">
+                       blade
                             @if($item->image_path)
                                 <img 
-                                    src="{{ $item->image_path }}" 
+                                    src="{{ env('S3_BUCKET_URL') }}/{{ $item->image_path }}" 
                                     alt="{{ $item->name }}" 
                                     class="max-h-full object-contain"
                                 >
-                                    alt="{{ $item->name }}" 
                             @else
                                 <div class="text-gray-400 text-xs">No Image</div>
                             @endif
-                        </div>
+    
 
                         {{-- Detail Text --}}
                         <div class="p-5 flex flex-col flex-grow">
